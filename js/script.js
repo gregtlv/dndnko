48a48ca412515817ab3fb8c4d4b3f85d258c20c4
Greg Mac <manov@manov.ru>
2018-04-11 23:41:03 +0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Greg 
+
 headerBurger.onclick = function expandMenu() {
   var headerNav = document.getElementById('headerNav');
 
@@ -13,5 +15,107 @@ headerBurger.onclick = function expandMenu() {
   }
 
   console.log(headerNavClass);
+
+}
+
+sendButton.onclick = function sendForm(event) { //event важная вещь для Firefox 
+
+	event.preventDefault();
+
+	var userName = document.getElementById('inputName');
+	var userNumber = document.getElementById('inputPhone');
+	var userMail = document.getElementById('inputEmail');
+	var userDate = document.getElementById('inputDate');
+	var userText = document.getElementById('inputText');
+
+	var alertMessage = document.getElementById('alertMessage');
+
+
+
+	let userName_Class = userName.classList;
+	let userNumber_Class = userNumber.classList;
+	let userMail_Class = userMail.classList;
+	let userDate_Class = userDate.classList;
+	let userText_Class = userText.classList;
+
+	let alertMessage_Class = alertMessage.classList;
+
+
+	if (userName.value == "") {
+		userName_Class.remove("regular__input");
+		userName_Class.add("red__input");
+
+	}
+	else {
+		userName_Class.remove("red__input");
+
+	}
+
+
+
+	if (userNumber.value == "") {
+		userNumber_Class.remove("regular__input");
+		userNumber_Class.add("red__input");
+
+	}
+	else {
+		userNumber_Class.remove("red__input");
+
+	}
+
+
+
+	if (userMail.value == "") {
+		userMail_Class.remove("regular__input");
+		userMail_Class.add("red__input");
+
+	}
+	else {
+		userMail_Class.remove("red__input");
+
+	}
+
+
+	
+	if (userDate.value == "") {
+		userDate_Class.remove("regular__input");
+		userDate_Class.add("red__input");
+
+	}
+	else {
+		userDate_Class.remove("red__input");
+
+	}
+
+	// Text Message
+
+	if (userText.value == "") {
+		userText_Class.remove("regular__input");
+		userText_Class.add("red__input");
+		console.log('Text mesage here');
+
+	}
+	else {
+		userText_Class.remove("red__input");
+
+	}
+
+
+	console.log(userName.classList);
+	console.log(userMail.classList);
+	console.log(userDate.classList);
+	console.log(userText.classList);
+
+
+	if (userName.value != "" && userNumber.value != "" && userMail.value != "" && userDate.value != "" && userText.value != "") {
+		// form.style.display = "none";
+		// classNamesForm.remove("form");
+		console.log('Form sent');
+
+		alert("Форма отправки пока что не подключена");
+	} else {
+		alertMessage.style.opacity = "1";
+			
+	}
 
 }
