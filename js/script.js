88c07173d54255558c6099ed7bf65629f533edca
Greg Mac <manov@manov.ru>
2018-04-18 12:42:38 +0300

    Many edits

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Greg
 
+// Navigation
+
 headerBurger.onclick = function expandMenu() {
   var headerNav = document.getElementById('headerNav');
 
   let headerNavClass = headerNav.classList;
 
   // console.log(headerNavClass);
-
 
   if (headerNav.className === "header-top__nav") {
       headerNav.className += " responsive";
